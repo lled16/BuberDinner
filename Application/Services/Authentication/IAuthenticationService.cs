4cc08ed813608a3d6efedc01d34558d595288666
Joao Simao <vitoralves1604@gmail.com>
2026-01-30 02:46:02 -0300

    Innitial commit

---
+++ Application/Services/Authentication/IAuthenticationService.cs
@@ -0,0 +1,16 @@
+﻿using Contracts.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Authentication
+{
+    public interface IAuthenticationService
+    {
+        AuthenticationResult Login(string email, string password);
+        AuthenticationResult Register(string firstName, string lastName, string email, string password);
+    }
+}
